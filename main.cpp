0afc4d05a9869a140133537880b110e91af3285b
benzhenwen <benzhenwen@gmail.com>
2025-03-22 02:32:19 -0500

    some experimenting pretty much

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include <GLFWE/texture.hpp>
 
 #include <GLFWE/text/character_set.hpp>
+#include <GLFWE/shape/quad.hpp>
 
 #include <chrono>
 #include <ctime>
@@ -18,10 +19,25 @@ int main() {
     const glm::ivec2 window_dimentions = {800, 800};
 
     // create window
-    auto & window = GLFWE::Window::create("Falling Everything", window_dimentions);
+    auto & window = GLFWE::Window::create("network", window_dimentions);
+
+    auto char_set = GLFWE::Text::CharacterSet("fonts/arial.ttf", 100);
 
     while(GLFWE::Window::has_instance()) {
+
+        window.clear_color();
+
+        GLFWE::Shape::ShapeShader::set_color({0, 1, 0});
+        GLFWE::Shape::Quad::draw_square({250, 250}, {300, 300});
+        GLFWE::Shape::ShapeShader::set_color({1, 0, 0});
+        GLFWE::Shape::Quad::draw_line({100, 100}, {700, 300}, 15);
+
+        char_set.render_string("meow", {10, 400}, 1.0f, {0.3f, 0.0f, 1.0f});
+        
+        window.swap_buffers();
+
         window.update();
+
     }
 
     
