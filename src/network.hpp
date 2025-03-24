0b627f1f3e932200368ebd303dcb134eebd35fa2
benzhenwen <benzhenwen@gmail.com>
2025-03-24 17:10:10 -0500

    refactor backpropagation into its own file

--- src/network.hpp
+++ src/network.hpp
@@ -4,11 +4,11 @@
 #include <algorithm>
 #include <vector>
 #include <random>
-#include <chrono>
 
 namespace BN {
 class Network {
-friend class NetworkDrawer;
+
+protected:
     static constexpr Logger logger = Logger("Network");
 
 public:
@@ -76,7 +76,7 @@ public:
         network.push_back(Layer(_input_layer_size));
     }
 
-    const std::vector<Layer> & access_network() {
+    std::vector<Layer> & layers() {
         return network;
     }
 
@@ -123,49 +123,6 @@ public:
     inline void calculate() {
         for (int layer_i = 1; layer_i < network.size(); layer_i++) {
             network[layer_i].calculate(network[layer_i-1]);
-        }
-    }
-
-    /*
-    adjusts weights and biases based off of expected values
-    you must set input_layer() and run calculate() first
-
-    shoutout to 3blue1brown <3 https://youtu.be/tIeHLnjs5U8?si=2c2cXLjppweIrZRa
-    */
-    inline void backpropagate(std::vector<float> & expected_values) {
-        auto expected_change = std::vector<float>(output_layer().size());
-
-        // init expected change first as difference in output vs expected
-        for (int i = 0; i < expected_change.size(); i++) {
-            expected_change[i] = output_layer()[i].value() - expected_values[i];
-        }
-
-        // logger << expected_change[0];
-
-        // main loop - from last layer to first (not including the input layer)
-        for (int layer_i = network.size() - 1; layer_i > 0; layer_i--) {
-            Layer & layer = network[layer_i];
-            auto prev_expected_change = std::vector<float>(network[layer_i-1].size());
-
-            for (int node_i = 0; node_i < layer.size(); node_i++) {
-                Node & node = layer[node_i];
-                const float d_error_d_value = sigmoid_prime(node.raw()) * 2 * expected_change[node_i]; // (dC / dz(L) = s'(z(L)) * 2 * (a(L) - y) 
-
-                // weights
-                for (int weight_i = 0; weight_i < node.weights.size(); weight_i++) {
-                    node.weights[weight_i] += -1 * network[layer_i-1][weight_i].value_check_is_input(network[layer_i-1].is_input_layer) * d_error_d_value; // -(dz(L) / dw(L)) * (dC / dz(L)
-                }
-
-                // bias
-                node.bias += -1 * d_error_d_value; // (dz(L) / db(L)) * (dC / dz(L))
-
-                // expected change of prev layer
-                for (int prev_i = 0; prev_i < network[layer_i-1].size(); prev_i++) {
-                    prev_expected_change[prev_i] += node.weights[prev_i] * d_error_d_value; // (dz(L) / da(L-1)) * (dC / dz(L))
-                }
-            }
-
-            expected_change = prev_expected_change;
         }
     }
 
